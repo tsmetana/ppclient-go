6895e5c6e5480cbcfe2bea90d46902a168ebeffd
Tomas Smetana <tomas@smetana.name>
2022-08-02 13:53:11 +0200

    GetLatestRelease implemented

--- pkg/ppclient/pprelease.go
+++ pkg/ppclient/pprelease.go
@@ -1,6 +1,11 @@
 package ppclient
 
-import "strings"
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 type PpRelease interface {
 	GetVersion() string
@@ -13,6 +18,8 @@ type PpRelease interface {
 }
 
 type PpReleaseList []PpRelease
+
+type ByVersion []PpRelease
 
 type ppRelease struct {
 	Shortname string `json:"shortname,omitempty"`
@@ -60,4 +67,60 @@ func (p *ppRelease) IsDeveloped() bool {
 
 func (p *ppRelease) IsZStream() bool {
 	return strings.HasSuffix(p.GetVersion(), ".z")
+}
+
+func (l *PpReleaseList) GetLatestVersion(includeZStream bool) string {
+	var i int
+	sort.Sort(ByVersion(*l))
+	for i = len(*l) - 1; i >= 0; i-- {
+		if !includeZStream && (*l)[i].IsZStream() {
+			continue
+		}
+		break
+	}
+	return (*l)[i].GetVersion()
+}
+
+func (r ByVersion) Len() int {
+	return len(r)
+}
+
+func (r ByVersion) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func normalizeVersionArray(arr *[]string) error {
+	if len(*arr) == 2 {
+		*arr = append(*arr, "0")
+	}
+	for idx, num := range *arr {
+		if num != "z" {
+			intVal, err := strconv.Atoi((*arr)[idx])
+			if err != nil {
+				return err
+			}
+			(*arr)[idx] = fmt.Sprintf("%3d", intVal)
+		}
+	}
+	return nil
+}
+
+func (r ByVersion) Less(i, j int) bool {
+	versionArr1 := strings.SplitN(r[i].GetVersion(), ".", 3)
+	versionArr2 := strings.SplitN(r[j].GetVersion(), ".", 3)
+	if len(versionArr1) < 2 || len(versionArr1) > 3 {
+		// Should never happen
+		return false
+	}
+	err := normalizeVersionArray(&versionArr1)
+	if err != nil {
+		return false
+	}
+	err = normalizeVersionArray(&versionArr2)
+	if err != nil {
+		return false
+	}
+	return (versionArr1[0] < versionArr2[0]) ||
+		((versionArr1[0] == versionArr2[0]) && (versionArr1[1] < versionArr2[1])) ||
+		((versionArr1[0] == versionArr2[0] && (versionArr1[1] == versionArr2[1])) && (versionArr1[2] < versionArr2[2]))
 }
